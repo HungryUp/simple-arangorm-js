9b72e21ce1a86616c5996ab4da10c8a9cc6c31c0
Gaël <gael@hungryup.co>
2017-08-16 11:08:48 +0200

    ✨ Experiments about simple ORM way

---
+++ model/document.js
@@ -0,0 +1,97 @@
+const genericModel = require('./arangoModel');
+
+const { db } = require('..');
+
+/**
+ * Create a model from schema provided
+ *
+ * @param schemaHandler
+ * @param options
+ * @returns {ArangoModel}
+ */
+module.exports = function (schemaHandler, options) {
+  const GenericModel = genericModel.call(this, schemaHandler, options);
+  options = Object.assign(
+    {
+      name: 'generic',
+      isEdge: false,
+      db,
+    },
+    options,
+  );
+  const ArangoDocumentModel = class ArangoDocumentModel extends GenericModel {
+    /**
+     * Get collection instance of arangoDB
+     */
+    static get collection() {
+      return options.db.collection(options.name);
+    }
+
+    /**
+     * Return cursor of entries
+     * @returns {Promise.<void>}
+     */
+    static async find(example, { offset, limit }) {
+      offset = offset || 0;
+      limit = limit || 100;
+      return (await ArangoDocumentModel.collection.byExample(example, { skip: offset, limit })).all();
+    }
+
+    /**
+     * Find first matching document in ArangoDB
+     *
+     * @param example
+     * @returns {Promise.<GenericModel>}
+     */
+    static async findOne(example) {
+      const data = await ArangoDocumentModel.collection.firstExample(example);
+      const object = new ArangoDocumentModel(data);
+      object.revision = data._rev;
+      object.key = data._key;
+      return object;
+    }
+
+    /**
+     * Find first document in ArangoDB with specified id
+     *
+     * @param id
+     * @param revision
+     * @returns {Promise.<GenericModel>}
+     */
+    static async findById(id, revision) {
+      const criteria = { _key: id };
+      if (revision) {
+        criteria._rev = revision;
+      }
+      return ArangoDocumentModel.findOne(criteria);
+    }
+
+    /**
+     * Update current document
+     *
+     * @param saveOptions
+     * @returns {Promise.<*>}
+     */
+    async save(saveOptions = {}) {
+      saveOptions = Object.assign({ returnNew: true }, saveOptions);
+
+      let result;
+      if (this._documentHandle) {
+        result = await ArangoDocumentModel.collection.update(this._documentHandle, this._validatedData, saveOptions);
+      } else {
+        result = await ArangoDocumentModel
+          .collection
+          .save(this._validatedData, saveOptions);
+      }
+
+      if (result.new) {
+        this.merge(result.new);
+      }
+      this.key = result._key;
+      this.revision = result._rev;
+      return result;
+    }
+  };
+  ArangoDocumentModel.setup();
+  return ArangoDocumentModel;
+};
---
+++ model/edge.js
@@ -0,0 +1,51 @@
+const genericModel = require('./arangoModel');
+
+const { db } = require('..');
+
+/**
+ * Create a model from schema provided
+ *
+ * @param schemaHandler
+ * @param options
+ * @returns {ArangoModel}
+ */
+module.exports = function (schemaHandler, options) {
+  const GenericModel = genericModel.call(this, schemaHandler, options);
+  options = Object.assign(
+    {
+      name: 'generic',
+      isEdge: false,
+      db,
+    },
+    options,
+  );
+  const ArangoEdgeModel = class ArangoEdgeModel extends GenericModel {
+    /**
+     * Get collection instance of arangoDB
+     */
+    static get collection() {
+      return options.db.edgeCollection(options.name);
+    }
+
+    async link(from, to, { unique } = {}) {
+      if (unique) {
+        const { updated } = await ArangoEdgeModel.collection.updateByExample(
+          { _from: from.id, _to: to.id },
+          this._validatedData || {},
+        );
+        if (updated) {
+          return this;
+        }
+      }
+      await ArangoEdgeModel.collection.save(this._validatedData || {}, from.id, to.id);
+      return this;
+    }
+
+    static async createUniqueLink(from, to, data) {
+      const o = new ArangoEdgeModel(data);
+      return o.link(from, to, { unique: true });
+    }
+  };
+  ArangoEdgeModel.setup();
+  return ArangoEdgeModel;
+};
