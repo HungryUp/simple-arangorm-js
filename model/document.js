67ea4c19b4937ef3b0944d277c0b1769e6e91408
Gaël <gael@hungryup.co>
2017-11-18 19:11:39 +0100

    🚧 Add WIP count & find

--- model/document.js
+++ model/document.js
@@ -1,3 +1,4 @@
+const qb = require('aqb');
 const AbstractModel = require('./abstract');
 
 module.exports = class AbstractDocument extends AbstractModel {
@@ -39,5 +40,28 @@ module.exports = class AbstractDocument extends AbstractModel {
   static async findOne(example) {
     const data = await this.collection.firstExample(example);
     return this.new.with(data);
+  }
+
+  /**
+   * Return cursor of entries
+   * @returns {Promise.<void>}
+   */
+  static async find(example = {}, { offset = 0, limit = 100 } = {}) {
+    const query = await this.collection.byExample(example, { skip: offset, limit });
+    const array = await query.all();
+    return array.map(o => this.new.with(o));
+  }
+
+  static async count(filters = {}) {
+    const COL_NAME = 'col';
+    const COUNTER = 'count';
+    let queryBuilder = qb.for(COL_NAME).in(this.collectionName);
+    if (this.buildCountFilters || this.buildFilters) {
+      queryBuilder = (this.buildCountFilters || this.buildFilters)(queryBuilder, filters);
+    }
+    queryBuilder = queryBuilder.collectWithCountInto(COUNTER).return(COUNTER);
+    const query = await this.query(queryBuilder);
+    const [count] = await query.all();
+    return count;
   }
 };
